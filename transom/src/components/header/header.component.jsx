fbea8048728f1abf7c041218bbe1254139abdd02
dbpatnodee <dbpatnode@hotmail.com>
2021-09-03 15:22:04 -0700

    moved hamburgerg

--- transom/src/components/header/header.component.jsx
+++ transom/src/components/header/header.component.jsx
@@ -11,6 +11,7 @@ function Header() {
 
     return (
         <nav className="header">
+            {/* Left side buttons (disapear on mobile) */}
             <ul className="nav-options">
                 <li>
                     <NavLink activeClassName="nav-active" to="/locations" className="option">
@@ -25,55 +26,56 @@ function Header() {
                     </NavLink>
                 </li>
             </ul>
-            {/* <div className='options'> */}
+
+            {/* right side buttons */}
             <ul className="right-nav">
                 <li className="logo"><img src="images/gsb-logo.png" alt="gsb logo"></img></li>
-                <li><div className={isActive ? "navigation active" : "navigation"} >
-                    <div className="ham-btn" onClick={ToggleClass}>
-                        <span></span>
-                        <span></span>
-                        <span></span>
+                <li>
+                    <div className={isActive ? "navigation active" : "navigation"} >
+                        <div className="ham-btn" onClick={ToggleClass}>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+
+                        {/* menu options when hamburger clicked */}
+                        <ul className="inner-left-nav-options">
+                            <li>
+                                <NavLink activeClassName="nav-active" className="inner-nav-option" to="/locations" >
+                                    <div className="flag"><img src="images/flag.png" alt="flag-icon"></img></div>
+                                    locations
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink activeClassName="nav-active" className="inner-nav-option" to="/order" >
+                                    <div className="car"><img src="images/car.png" alt="car-icon"></img></div>
+                                    order online
+                                </NavLink>
+                            </li>
+                        </ul>
+                        <ul className="option">
+                            <li className="inner-logo"><img src="images/gsb-logo.png" alt="gsb logo"></img></li>
+                        </ul>
+                        <div className="links">
+                            <div className="link">
+                                <NavLink activeClassName="nav-active" exact to="/">Home</NavLink>
+                            </div>
+                            <div className="link">
+                                <NavLink activeClassName="nav-active" to="/locations">Locations</NavLink>
+                            </div>
+                            <div className="link">
+                                <NavLink activeClassName="nav-active" to="/menu" >Menu</NavLink>
+                            </div>
+                            <div className="link">
+                                <NavLink activeClassName="nav-active" to="/about">About</NavLink>
+                            </div>
+                            <div className="link">
+                                <NavLink activeClassName="nav-active" to="/jobs">Jobs</NavLink>
+                            </div>
+                        </div>
                     </div>
-                    {/* <span class="inner-disappeared-buttons"> */}
-                    <ul className="inner-left-nav-options">
-                        <li>
-                            <NavLink activeClassName="nav-active" className="inner-nav-option" to="/locations" >
-                                <div className="flag"><img src="images/flag.png" alt="flag-icon"></img></div>
-                                locations
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink activeClassName="nav-active" className="inner-nav-option" to="/order" >
-                                <div className="car"><img src="images/car.png" alt="car-icon"></img></div>
-                                order online
-                            </NavLink>
-                        </li>
-                    </ul>
-                    {/* </span> */}
-                    <ul className="option">
-                        <li className="inner-logo"><img src="images/gsb-logo.png" alt="gsb logo"></img></li>
-                    </ul>
-                    <div className="links">
-                        <div className="link">
-                            <NavLink activeClassName="nav-active" exact to="/">Home</NavLink>
-                        </div>
-                        <div className="link">
-                            <NavLink activeClassName="nav-active" to="/locations">Locations</NavLink>
-                        </div>
-                        <div className="link">
-                            <NavLink activeClassName="nav-active" to="/menu" >Menu</NavLink>
-                        </div>
-                        <div className="link">
-                            <NavLink activeClassName="nav-active" to="/about">About</NavLink>
-                        </div>
-                        <div className="link">
-                            <NavLink activeClassName="nav-active" to="/jobs">Jobs</NavLink>
-                        </div>
-                    </div>
-                </div>
                 </li>
             </ul>
-            {/* </div> */}
         </nav >
     )
 }
