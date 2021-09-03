216cfa5f7e82f598152487fc221bfac666558d5a
dbpatnodee <dbpatnode@hotmail.com>
2021-09-03 15:21:46 -0700

    closer to design specs

--- transom/src/components/header/header.component.jsx
+++ transom/src/components/header/header.component.jsx
@@ -26,7 +26,7 @@ function Header() {
                 </li>
             </ul>
             {/* <div className='options'> */}
-            <ul className="signin-up">
+            <ul className="right-nav">
                 <li className="logo"><img src="images/gsb-logo.png" alt="gsb logo"></img></li>
                 <li><div className={isActive ? "navigation active" : "navigation"} >
                     <div className="ham-btn" onClick={ToggleClass}>
@@ -34,22 +34,22 @@ function Header() {
                         <span></span>
                         <span></span>
                     </div>
-                    <span class="inner-disappeared-buttons">
-                        <ul>
-                            <li>
-                                <NavLink activeClassName="nav-active" to="/locations" className="inner-left-buttons">
-                                    <div className="flag"><img src="images/flag.png" alt="flag-icon"></img></div>
-                                    <div className="nav-text">locations</div>
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink activeClassName="nav-active" to="/order" className="inner-left-buttons">
-                                    <div className="car"><img src="images/car.png" alt="car-icon"></img></div>
-                                    <div className="nav-text">order online</div>
-                                </NavLink>
-                            </li>
-                        </ul>
-                    </span>
+                    {/* <span class="inner-disappeared-buttons"> */}
+                    <ul className="inner-left-nav-options">
+                        <li>
+                            <NavLink activeClassName="nav-active" className="inner-nav-option" to="/locations" >
+                                <div className="flag"><img src="images/flag.png" alt="flag-icon"></img></div>
+                                locations
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink activeClassName="nav-active" className="inner-nav-option" to="/order" >
+                                <div className="car"><img src="images/car.png" alt="car-icon"></img></div>
+                                order online
+                            </NavLink>
+                        </li>
+                    </ul>
+                    {/* </span> */}
                     <ul className="option">
                         <li className="inner-logo"><img src="images/gsb-logo.png" alt="gsb logo"></img></li>
                     </ul>
